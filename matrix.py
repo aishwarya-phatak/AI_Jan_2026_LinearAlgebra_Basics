24c6b68d27a4d477e5917468118d6abc1f4cee8c
aishwarya <ashp17star@gmail.com>
2026-02-14 12:26:47 +0530

    matrix implementation - addition, multiplication, simple linear regression ML algorithm

--- matrix.py
+++ matrix.py
@@ -4,10 +4,10 @@ import numpy as np
 v1 = np.array([11, 22, 33])
 print(v1)
 #matrix
-m1 = np.array([[21,23],[24,19]])
+m1 = np.array([[1,1],[1,1]])
 print(m1)
 
-m2 = np.array([[19,13],[18,25]])
+m2 = np.array([[2,2],[2,2]])
 print(m2)
 
 print(m1 + m2)         #matrix addition
@@ -23,3 +23,22 @@ print(mat_i)
 #zeros matrix
 mat_z = np.zeros((3,3))
 print(mat_z)
+
+print("-----------------")
+#matrix multiplication - dot product
+mat_mul_res = np.dot(m1,m2)
+print(mat_mul_res)
+
+#implementation of ML simple algorithm -- linear regression
+X = np.array([[13,19],[16,15]])
+print(X)
+Y = np.array([2,0.05])
+b = 0
+prediction1 = np.dot(X,Y) + b
+print("--------------")
+print(prediction1)
+
+
+A = np.array([[1,1],[1,1]])
+result = 3 * A
+print(result)
